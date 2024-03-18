f1acfc6d2dc857e0dd3ecf5278aa66aca407d6c2
Usuario <Usuario@CPD03-M08>
2024-03-18 12:27:37 -0300

    Edição de Main Page

--- MauiAppListaCompras/MainPage.xaml.cs
+++ MauiAppListaCompras/MainPage.xaml.cs
@@ -1,12 +1,99 @@
-﻿namespace MauiAppListaCompras
+﻿using System.Collections.ObjectModel;
+using MauiAppListaCompras.Models;
+
+namespace MauiAppListaCompras
 {
     public partial class MainPage : ContentPage
     {
+        
+        ObservableCollection<Produto> lista_produtos =
+            new ObservableCollection<Produto>();
+
         public MainPage()
         {
             InitializeComponent();
+            lst_produtos.ItemsSource = lista_produtos;
         }
 
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            double soma = lista_produtos.Sum(i => (i.Preco * i.Quantidade));
+            string msg = $"O total é {soma:C}";
+            DisplayAlert("Somatória", msg, "Fechar") ;
+        }
+
+        protected override void OnAppearing()
+        {
+            if(lista_produtos.Count == 0) 
+            {
+                Task.Run(async () =>
+                {
+                    List<Produto> tpm = await App.Db.GetAll();
+                    foreach (Produto p in tpm)
+                    {
+                        lista_produtos.Add(p);
+                    }
+                }); //Fecha task                  
+            }// Fecha 
+        }
+
+        private async void ToolbarItem_Clicked_Add(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//NovoProduto");
+        }
+
+        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string q = e.NewTextValue;
+            lista_produtos.Clear();
+            Task.Run(async () =>
+            {
+                List<Produto> tmp = await App.Db.Seach(q);
+                foreach (Produto p in tmp)
+                {
+                    lista_produtos.Add(p);
+                }
+            });
+        }
+
+        private void ref_carregando_Refreshing(object sender, EventArgs e)
+        {
+            lista_produtos.Clear();
+            Task.Run(async () =>
+            {
+                List<Produto> tmp = await App.Db.GetAll();
+                foreach (Produto p in tmp)
+                {
+                    lista_produtos.Add(p);
+                }
+            });
+            ref_carregando_IsRefreshing = false;
+        }
+
+        private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                MenuItem selecionado = (MenuItem)sender;
+
+                Produto p = selecionado.BindingContext as Produto;
+                bool confirm = await DisplayAlert(
+                    "Tem certeza?", "Remover Produto?",
+                    "Sim", "Cancelar");
+
+                if (confirm) 
+                {
+                    await App.Db.Delete(p.Id);
+                    await DisplayAlert("Sucesso!",
+                        "Produto Removido", "Ok");
+                }
+            }
+        }
     }
 
 }
